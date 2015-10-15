f691b4ce8fc1a26d123c204db329d3162d435ab4
gaurav gupta <gaurav_bng@hotmail.com>
2015-10-15 18:46:08 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,20 +1,37 @@
 'use strict';
 
-var express = require('express'),
-    Db = require('./config/db'),
-    config = require('./config/config');
-var bodyParser = require('body-parser');
+var express = require('express');
 
 var app = express();
 
+var server = require('http').createServer(app);  
+
+var io = require('socket.io')(server);
+
 app.use(express.static(__dirname + '/../client/src'));
 
-app.use(bodyParser());
+server.listen(8000);
 
-require('./routes')(app);
+console.log('Express app started on port ' + 8000);
 
-var port = config.server.port;
+var online = 0;
 
-app.listen(port);
+io.on('connection', function(client) {  
+    client.on('join', function(data) {
+    	online++;
+        console.log(online+ " "+data);        
+    });
 
-console.log('Express app started on port ' + port);
+    client.on('messages', function(data) {
+    		console.log(data);
+           	client.emit('broad', data);
+           	client.broadcast.emit('broad',data);
+    });
+
+    client.on('disconnect', function () {
+    	online = online -1;
+    	console.log("User got disconnected..");
+      	console.log(online +" users are live");
+  });
+
+});
