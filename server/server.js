224a74fd5caec5c6b1feb8a07ff72a60a4d93e92
gaurav gupta <gaurav_bng@hotmail.com>
2015-10-19 14:46:18 +0530

    improved UI and implemented chat with user name intact

--- server/server.js
+++ server/server.js
@@ -23,14 +23,19 @@ io.on('connection', function(client) {
     });
 
     client.on('messages', function(data) {
-          client.emit('broad', data);
-          client.broadcast.emit('broad',data);
+          client.emit('broad', data);   // emmit message for onself;
+          client.broadcast.emit('broad',data); //emit message for others, i.e broadcast
+    });
+
+    client.on('chat_content', function(user_name, chat_content) {
+          client.emit('brod_chat_content', user_name, chat_content); // emmit message for onself;
+          client.broadcast.emit('brod_chat_content', user_name, chat_content); //emit message for others, i.e broadcast
     });
 
     client.on('disconnect', function () {
-    	online = online -1;
-    	console.log("User got disconnected..");
-      	console.log(online +" users are live");
+    	   online = online -1;
+    	   console.log("User got disconnected..");
+      	 console.log(online +" users are live");
   });
 
 });
